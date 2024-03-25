4b9ef6cf5ca9ec445b67ae69d697815e25eb65a4
javierandrango <lab_iarobotico593@outlook.com>
2024-03-25 00:28:56 -0500

    ESP32Server: send data from web page to esp32 with websocket

--- CodeTesting/ESP32Server/data/static/js/main.js
+++ CodeTesting/ESP32Server/data/static/js/main.js
@@ -1,7 +1,23 @@
+/**
+ * Variable declaration
+ * ---------------------------------------------------------------------
+ */
+//HTML
 let toggle = document.getElementById("toggleButton");
 let esp32LedStatus = document.getElementById("ESP32Led");
+//web socket URL
+var websocket;
+let gateway = `ws://${window.location.hostname}/ws`;
+//----------------------------------------------------------------------
+
+
+/**
+ * Event listener
+ * ---------------------------------------------------------------------
+ */
 
 if (toggle){
+    let ledState = "";
     toggle.addEventListener("click",()=>{
         if (toggle.classList.contains("btn-secondary")){
             //toggle button color
@@ -10,12 +26,68 @@ if (toggle){
             //change led status name
             toggle.innerHTML = "LED ON"
             //esp32LedStatus.value = "ON"
+            ledState = "ON"
         }
         else{
             toggle.classList.remove("btn-success");
             toggle.classList.add("btn-secondary");
             toggle.innerHTML = "LED OFF"
             //esp32LedStatus.value = "OFF"
+            ledState = "OFF"
         }
+        sendData(ledState);
     })
 }
+
+
+window.addEventListener("load",()=>{
+    createWebSocket();
+});
+//----------------------------------------------------------------------
+
+
+/**
+ * User functions
+ * ---------------------------------------------------------------------
+ */
+
+
+function createWebSocket(){
+    console.log("Trying to open a WebSocket connection...");
+    //create webSocket
+    websocket = new WebSocket(gateway);
+    
+    //connection openned
+    websocket.addEventListener("open",(event)=>{
+        console.log("webSocket connection openned");
+    });
+
+    //listen for errors
+    websocket.addEventListener("error",(event)=>{
+        if(event){
+            console.log("webSocket error")
+        }
+    });
+
+    //attemp to reconnect after a short time
+    websocket.addEventListener("close",(event)=>{
+        console.log("webSocket connection closed");
+        setTimeout(createWebSocket,2000);    
+    });
+}
+
+
+function sendData(data){
+    //check if the webSocket is on OPEN state (state 1 is OPEN)
+    //more info: developer.mozilla.org
+    if(websocket.readyState === WebSocket.OPEN){
+        websocket.send(data);
+        console.log("data send to ESP32");
+    }
+    else{
+        console.log("WebSocket connection is not open");
+    }
+}
+
+
+//----------------------------------------------------------------------
--- CodeTesting/ESP32Server/src/main.cpp
+++ CodeTesting/ESP32Server/src/main.cpp
@@ -8,16 +8,22 @@
 /*-------------------------------------------------------------------*/
 
 //Build-in led
+/*
 int interval_time = 1000;// miliseconds
 unsigned long current_time = 0;
 unsigned long previous_time = 0;
 bool led_state = LOW;
+*/
+bool ledState = LOW;
 
 //Web server credentials
-const char* ssid = "CELERITY_ANDRANGO";
+const char* ssid = "CELERITY_FLIA_ANDRANGO";
 const char* password = "091992javier";
 AsyncWebServer server(80);
-const char* PARAM_MESSAGE = "message";
+//const char* PARAM_MESSAGE = "message";
+
+//webSocket in /ws path
+AsyncWebSocket ws("/ws");
 
 //LittleFs filesystem
 #define FORMAT_LITTLEFS_IF_FAILED true
@@ -30,9 +36,13 @@ const char* PARAM_MESSAGE = "message";
 //blink led
 //void blinkLed(int);
 
-//HTML request
+// HTML request
 void notFound(AsyncWebServerRequest *request);
-
+// message received from webSocket
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len);
+// webSocket event
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len);
 /*-------------------------------------------------------------------*/
 
 
@@ -44,7 +54,7 @@ void setup() {
   Serial.begin(115200);
 
   //Build-in led
-  //pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
 
   
   //Web server as Station mode:
@@ -78,7 +88,7 @@ void setup() {
         request->send(LITTLEFS,"/index.html","text/html", false);
   });
   
-
+  /*
   // Send a GET request to <IP>/get?message=<message>
     server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
         String message;
@@ -89,8 +99,13 @@ void setup() {
         }
         request->send(200, "text/plain", "Hello, GET: " + message);
     });
-  
-  //server request: page not found
+  */
+
+  // initialize webSocket
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+
+  // server request: page not found
   server.onNotFound(notFound);
   server.begin();
   
@@ -103,6 +118,8 @@ void setup() {
 /*-------------------------------------------------------------------*/
 void loop() {
   //blinkLed(interval_time);
+  ws.cleanupClients();
+  digitalWrite(BUILTIN_LED,ledState);
 }
 /*-------------------------------------------------------------------*/
 
@@ -111,29 +128,65 @@ void loop() {
 /*put function definitions here:*/
 /*-------------------------------------------------------------------*/
 // blik led 
-void blinkLed(int time_ms){
-  /**
-   * n: number of times the led will blink
-   * time_ms: time perior between blinks
-  */
-  current_time = millis();
-  if (current_time - previous_time>=time_ms){
-    //change state
-    if (led_state == LOW){
-      led_state = HIGH;
-    }
-    else{
-      led_state = LOW;
-    }
-    //update time
-    previous_time = current_time;
-  }
-  digitalWrite(BUILTIN_LED, led_state);
-}
+
+//void blinkLed(int time_ms){
+//  /**
+//   * n: number of times the led will blink
+//   * time_ms: time perior between blinks
+//  */
+//  current_time = millis();
+//  if (current_time - previous_time>=time_ms){
+//    //change state
+//    if (led_state == LOW){
+//      led_state = HIGH;
+//    }
+//    else{
+//      led_state = LOW;
+//    }
+//    //update time
+//    previous_time = current_time;
+//  }
+//  digitalWrite(BUILTIN_LED, led_state);
+//}
 
 // html not found 
 void notFound(AsyncWebServerRequest *request) {
     request->send(404, "text/plain", "Not found");
 }
+
+// message received from webSocket
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    // return 0 if data comparison is equal
+    if (strcmp((char*)data, "ON") == 0) {
+      ledState = HIGH;
+    }
+    else if (strcmp((char*)data, "OFF") == 0){
+      ledState = LOW;
+    }
+  }
+}
+
+// webSocket event
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
 
 /*-------------------------------------------------------------------*/
